2114ece5293caa7615854939fe9a6f3a78e5f720
V <V>
2022-07-31 11:52:22 +0400

    create MovieTableViewController

---
+++ MovieCoreData/Controller/MovieTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieTableViewController.swift
+//  MovieCoreData
+//
+//  Created by Apple on 31.07.2022.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as! MovieTableViewCell
+
+        return cell
+    }
+    
+}
