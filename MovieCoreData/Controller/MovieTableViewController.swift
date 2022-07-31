a59e2f8914cbc4bac58f334c5f68abf0200411ef
V <V>
2022-07-31 14:44:28 +0400

    tableView DataSource

--- MovieCoreData/Controller/MovieTableViewController.swift
+++ MovieCoreData/Controller/MovieTableViewController.swift
@@ -6,30 +6,42 @@
 //
 
 import UIKit
+import CoreData
 
 class MovieTableViewController: UITableViewController {
+    
+    private var coreData = CoreDataStack()
+    private var fetchedResultController: NSFetchedResultsController<Movie>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        guard let sections = fetchedResultController?.sections else { return 0 }
+        return sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 2
+        guard let sections = fetchedResultController?.sections else { return 0 }
+        return sections[section].numberOfObjects
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell", for: indexPath) as! MovieTableViewCell
-
+        if let movie = fetchedResultController?.object(at: indexPath) {
+            cell.configureCell(movie)
+        }
         return cell
     }
     
-}
+    //MARK: - Private functions
+    
+    private func loadData() {
+        fetchedResultController = MovieService.getMovies(moc: coreData.persistentContainer.viewContext)
+    }
+ }
--- MovieCoreData/View/MovieTableViewCell.swift
+++ MovieCoreData/View/MovieTableViewCell.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    
     @IBOutlet weak var movieImageView: UIImageView!
     @IBOutlet weak var movieTitleLabel: UILabel!
     @IBOutlet weak var movieFormatLabel: UILabel!
     @IBOutlet weak var userRating: UserRating!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
     func configureCell(_ movie: Movie) {
         movieTitleLabel.text = movie.title
@@ -32,6 +22,5 @@ class MovieTableViewCell: UITableViewCell {
         if let imageData = movie.image {
             movieImageView.image = UIImage(data: imageData)
         }
-        
     }
 }
